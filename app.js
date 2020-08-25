4fa75cfcaf42fb0eeded3f90398ce8f68c3e52a9
luongdangvp2000 <48552663+luongdangvp2000@users.noreply.github.com>
2020-08-25 23:27:32 +0700

    add animation next page

--- app.js
+++ app.js
@@ -12,32 +12,20 @@ var FadeTransition = Barba.BaseTransition.extend({
     .all([this.newContainerLoading, this.fadeOut()])
     .then(this.fadeIn.bind(this));
     },
+
     fadeOut: function() {
-    /**
-* this.oldContainer is the HTMLElement of the old Container
-*/
-    return $(this.oldContainer).animate({ opacity: 0 }).promise();
+        
     },
+
     fadeIn: function() {
-    /**
-* this.newContainer is the HTMLElement of the new Container
-* At this stage newContainer is on the DOM (inside our #barba-container and with visibility: hidden)
-* Please note, newContainer is available just after newContainerLoading is resolved!
-*/
-    var _this = this;
-    var $el = $(this.newContainer);
-    $(this.oldContainer).hide();
-    $el.css({
-    visibility : 'visible',
-    opacity : 0
-    });
-    $el.animate({ opacity: 1 }, 400, function() {
-    /**
-* Do not forget to call .done() as soon your transition is finished!
-* .done() will automatically remove from the DOM the old Container
-*/
-    _this.done();
-    });
+        this.newContainer.classList.add('slide-in');
+
+        let that= this;
+
+        this.newContainer.addEventListener('animationed', function(){
+            that.newContainer.classList.remove('slide-in');
+            that.done();
+        });
     }
 });
 /**
